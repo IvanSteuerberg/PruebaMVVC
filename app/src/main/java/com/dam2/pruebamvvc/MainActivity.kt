e32b0eb2c5589a50fa4770d558d4630a61096a58
IvanSteuerberg <irodriguezsteuerberg@danielcastelao.org>
2020-11-16 10:43:34 +0100

    Cambios, sumarRonda funcional

--- app/src/main/java/com/dam2/pruebamvvc/MainActivity.kt
+++ app/src/main/java/com/dam2/pruebamvvc/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+//para no usar findViewById
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +16,15 @@ class MainActivity : AppCompatActivity() {
         // nomenclatura que necesita utilizar jvm 1.8
         // se configure en project structure -> Modules -> Target Compatibillity
         val miModelo by viewModels<MyViewModel>()
-        miModelo.ronda.value
+        //miModelo.ronda.value
         val texto = findViewById<TextView>(R.id.textRonda)
+        //texto.text = miModelo.ronda.value.toString()
+        miModelo.ronda.observe(this, Observer{
+        nuevaRonda -> textRonda.text = nuevaRonda.toString()
+        })
+        sumarRonda.setOnClickListener(){
+            miModelo.sumarRonda()
+        }
 
     }
 }
